4c6da7ecfe67eaf36a03415a13fc326dd4b0fe3d
Rahul Yadav <rahul3586yadav@gmail.com>
2023-12-21 22:58:55 +0530

    New commit

--- Methods.java
+++ Methods.java
@@ -1,9 +1,10 @@
 public class Methods {
+
+    public static void main (String args){
+        add(3,5);  
+    }
     public static void add (int a, int b){
         int sum = a+b;
         System.out.println(sum);
-    }
-    public static void main (String args){
-        add(3,5);  
     }
 }
