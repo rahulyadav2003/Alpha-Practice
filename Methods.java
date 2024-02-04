3596508bcc365dae08798dbf61ea49d496576bd1
Rahul Yadav <rahul3586yadav@gmail.com>
2024-02-04 10:57:36 +0530

    hello

--- Methods.java
+++ Methods.java
@@ -1,10 +1,36 @@
-public class Methods {
+import java.util.*;
+// //Firsst Function/Methods
 
-    public static void main (String args){
-        add(3,5);  
+class Methods{
+
+    public static void demo(){
+        System.out.println("Hello World");
+        return;
     }
-    public static void add (int a, int b){
+
+    public static int add(int a, int b){ // A and B are parameters or formal parameter
         int sum = a+b;
-        System.out.println(sum);
+        return sum;
+    }
+
+    public static void swap (int a, int b){
+        int temp;
+        temp = a;
+        a = b;
+        b = temp; 
+        
+    System.out.println("Swap Value A : " + a);
+    System.out.println("Swap Value B : " + b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("A = ");
+        int a = sc.nextInt();
+        System.out.print("B = ");
+        int b = sc.nextInt();
+        swap(a,b);
+        //System.out.print("Sum = " + add(a,b));  
+        sc.close();
     }
 }
