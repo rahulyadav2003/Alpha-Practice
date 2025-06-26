873c0748cf9c7fe278d8864a930d51c50a3e2cdb
Rahul Yadav <rahul3586yadav@gmail.com>
2025-06-26 22:56:26 +0530

    commit

--- patterns.java
+++ patterns.java
@@ -1,14 +1,14 @@
 public class patterns {
     public static void main(String args[]){
         // char ch = 'A';
-        int num = 1;
-        for (int i=1; i<=5;i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(num+" ");
-                num++;
-            }
-            System.out.println();
-         }
+        // int num = 1;
+        // for (int i=1; i<=5;i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(num+" ");
+        //         num++;
+        //     }
+        //     System.out.println();
+        //  }
 
         //inverted star pattern
 
@@ -18,5 +18,13 @@ public class patterns {
         //     }
         //     System.out.println();
         // }
+
+        int n =4;
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }  
 }
